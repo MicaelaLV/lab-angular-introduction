1a29f6e059bf24b604e5ebc51707174b5edef69f
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-29 23:58:52 +0200

    started app and finished iteration 1

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  message: string = "Photo Album";
+  picture1: string = 'https://media.giphy.com/media/3ohzdUi5U8LBb4GD4s/giphy.gif';
+  caption1: string = 'Jon Snow';
+  picture2: string = 'https://media.giphy.com/media/3o7bu1PjAacbngGcpO/giphy.gif';
+  caption2: string = 'Winter has come';
+  picture3: string = 'https://media.giphy.com/media/NtX2BPM2NsTQs/giphy.gif';
+  caption3: string = 'in a Lover\'s quarrel';
 }
